bed24eb32793f0e451aea4c7e3bc7fba09bdf8e8
pio95 <pio_95@10g.pl>
2016-01-25 20:05:00 +0100

    Zmiany w interfejs.tex

--- view/logowanie.php
+++ view/logowanie.php
@@ -1,18 +1,18 @@
 <h2>Klient - logowanie</h2>
 <p>Aby zalogować się na konto, wprowadź login i hasło</p>
 <form class="formularz" action="login.html" method="post" >
-<ul>
-<li>
-<label for="login">Login</label>
-<input type="text" name="login" value="<?php if(isset($_POST['login']))echo $_POST['login']; ?>" >
-</li>
-<li>
-<label for="password">Hasło</label>
-<input type="password" name="password" value="<?php if(isset($_POST['password'])) echo $_POST['password']; ?>" >
-</li>
-<li>
-<input type="submit">
-</li>
-</ul>
+  <ul>
+	<li>
+		<label for="login">Login</label>
+		<input type="text" name="login" value="<?php if(isset($_POST['login']))echo $_POST['login']; ?>" >
+	</li>
+	<li>
+		<label for="password">Hasło</label>
+		<input type="password" name="password" value="<?php if(isset($_POST['password'])) echo $_POST['password']; ?>" >
+	</li>
+	<li>
+		<input type="submit">
+	</li>
+  </ul>
 </form>
 <br>
