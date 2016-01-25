c2b638c3416254fbc1186b199eea8d1ab61376ce
pio95 <pio_95@10g.pl>
2016-01-25 14:46:04 +0100

    Fix na podwójne logowanie

--- ctrl/login-pracownik.php
+++ ctrl/login-pracownik.php
@@ -5,6 +5,7 @@ $password = $DB->real_escape_string(gen_hash($_POST['password']));
 $result = $DB->query("SELECT pracownik_id FROM pracownik WHERE login='$login' AND haslo='$password'") or DBdie($DB->error);
 if($result && $row = $result->fetch_row()) {
 	$_SESSION['pracownik_id']=$row[0];
+	if(isset($_SESSION['klient_id'])) unset($_SESSION['klient_id']);
 	putMessage('Zostałeś zalogowany');
 	header("Location: {$dir}glowna.html");
 	exit;
--- ctrl/login.php
+++ ctrl/login.php
@@ -5,6 +5,7 @@ $password = $DB->real_escape_string(gen_hash($_POST['password']));
 $result = $DB->query("SELECT klient_id FROM klient WHERE login='$login' AND haslo='$password'") or DBdie($DB->error);
 if($result && $row = $result->fetch_row()) {
 	$_SESSION['klient_id']=$row[0];
+	if(isset($_SESSION['pracownik_id'])) unset($_SESSION['pracownik_id']);
 	putMessage('Zostałeś zalogowany');
 	header("Location: {$dir}glowna.html");
 	exit;
